11491f4998040393b61265b39948ccd72a3195ef
Neil McAlister <neil.mcalister@futurice.com>
2016-12-25 14:01:46 +0200

    Add project files.

---
+++ FridgeShoppingList/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using FridgeShoppingList.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace FridgeShoppingList.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
