60a9e26045e80baa7e08122b821ac02b4d5a8728
Neil McAlister <neil.mcalister@futurice.com>
2017-01-04 18:16:16 +0200

    Some basic plumbing, trying to get RX to work

---
+++ FridgeShoppingList/Models/GroceryItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FridgeShoppingList.Models
+{
+    [DebuggerDisplay("Name: {Name}, Expires: {ExpiryDate?.ToString()}")]
+    public class GroceryItem
+    {
+        public GroceryItemType ItemType { get; set; }
+        public DateTime? ExpiryDate { get; set; }
+    }
+}
---
+++ FridgeShoppingList/ViewModels/ControlViewModels/InventoryEntryViewModel.cs
@@ -0,0 +1,27 @@
+﻿using FridgeShoppingList.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template10.Mvvm;
+
+namespace FridgeShoppingList.ViewModels.ControlViewModels
+{
+    public class InventoryEntryViewModel : BindableBase
+    {
+        public ObservableCollection<GroceryItem> Items { get; set; }
+
+        public InventoryEntryViewModel(GroceryItem item)
+        {
+            Items = new ObservableCollection<GroceryItem>(); 
+            Items.Add(item);
+        }
+
+        public InventoryEntryViewModel(IEnumerable<GroceryItem> items)
+        {
+            Items = new ObservableCollection<GroceryItem>(items);
+        }
+    }
+}
